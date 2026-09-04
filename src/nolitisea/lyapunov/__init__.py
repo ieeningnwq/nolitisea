cd5d46cb50fc7f371aa4732a5fdc25991d31680c
wenqingNie <1258820178@qq.com>
2026-09-04 19:06:38 +0800

    修改 lyapunov/init文件

--- src/nolitisea/lyapunov/__init__.py
+++ src/nolitisea/lyapunov/__init__.py
@@ -1,3 +1,3 @@
-"""Lyapunov exponent estimation: Kantz, Rosenstein, full spectrum."""
+"""Lyapunov exponent estimation: Kantz, Rosenstein, full spectrum, FSLE."""
 
-from . import lyap_k, lyap_r, lyap_spec  # noqa: F401
+from . import lyap_k, lyap_r, lyap_spec
